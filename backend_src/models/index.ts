a8bdd2434254f2a02056a564a3961cd0d4be7e39
gtsorho <georgesorho@gmail.com>
2024-05-30 07:26:59 +0000

    Remove .env file from repository

--- backend_src/models/index.ts
+++ backend_src/models/index.ts
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD!, 
     {
         host: process.env.DB_HOST!,
+        port:3306,
         dialect: 'mysql',
         logging: false
     }
